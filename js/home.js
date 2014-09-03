1c48a0d262bbe26e0a599069a291491e7bedee58
bhirsch42 <benhirsch42@gmail.com>
2014-09-03 03:43:21 -0400

    More small polishes for mobile.

--- js/home.js
+++ js/home.js
@@ -96,7 +96,7 @@ $('a.expand-article').click(function() {
 			'width': "100%",
 			'height': "100%",
 			"border-radius":"0",
-			// "left": "0",
+			"left": "0",
 			"margin-left": "0",
 			"margin-right": "0",
 			"z-index": 100
